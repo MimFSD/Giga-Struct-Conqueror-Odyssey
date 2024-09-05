b7b861449de13d13b62bfd753a9b09ad60389207
MimFSD <mimislam878@gmail.com>
2024-09-06 01:59:56 +0600

    variable & Identifier

---
+++ Variable, Data Types & Operators/variable.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int a=6 , b=2 ,c=9;
+    cout << "value1 : " << a << "    value2 : "<< b << "    value3 : " << c ;
+
+}
+
+// identifier can be described as the user-specified name used to identify a specific item.

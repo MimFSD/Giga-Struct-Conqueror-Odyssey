cf0543b65cc9124c08e1da5f1826d17dff192b2b
MimFSD <mimislam878@gmail.com>
2024-09-06 03:48:04 +0600

    sum of input numbers

---
+++ Variable, Data Types & Operators/sum-of-input-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int c ,d ;
+    cout<<"Enter C : " ;
+        cin>>c  ;
+    cout<<"Enter D : " ;
+      cin>>d;
+
+    int sum = c+d;
+    cout << "Sum is : " << sum ;
+
+  return 0;
+}

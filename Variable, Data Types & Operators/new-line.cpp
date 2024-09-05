70d11a12b904fee8f68965c947736f5fcd00851e
MimFSD <mimislam878@gmail.com>
2024-09-06 01:06:54 +0600

    added end line

---
+++ Variable, Data Types & Operators/new-line.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+    cout << "standard library header file for input output \n streams that It includes functionality to read and write from streams "<< endl << "namespace is a collection of related names or identifiers (functions, class, variables) which helps to separate these identifiers from similar " ;
+
+}
+
+//  \n -- New line --- Moves the cursor to the start of the next line.

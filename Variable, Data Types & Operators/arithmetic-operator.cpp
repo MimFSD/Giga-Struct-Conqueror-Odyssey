c8f13dfba11f70fe45a2f702e59c9629039c4ca2
MimFSD <mimislam878@gmail.com>
2024-09-06 03:25:14 +0600

    arithmetic operators

---
+++ Variable, Data Types & Operators/arithmetic-operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main () {
+ int p=44,q=7;
+
+ cout<< "sum : " << p+q << endl;
+ cout<< "insertion : " << p-q << endl;
+ cout<< "Multiplication : " << p*q << endl;
+ cout<< "Division : " << p/q << endl;
+ cout<< "Remainder : " << p%q << endl;
+  return 0;
+}

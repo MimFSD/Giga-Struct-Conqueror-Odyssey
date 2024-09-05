c60cd339c9014135ab53a35ff6fe0b7db340f34a
MimFSD <mimislam878@gmail.com>
2024-09-06 02:35:54 +0600

    Data Types

---
+++ Variable, Data Types & Operators/data-type.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int a = 375 ;               // 2 bytes 
+    float b = 34.7654675 ;          // 7 decimal digits precision // 4 bytes
+    double c = 64.756427338 ;            // 14 decimal digits precision // 8 bytes
+    char Grade = 'A';           // 1  bytes
+    bool isSafe = true  ;           // 1 bytes
+
+    cout << "value1 : " << a << "    value2 : "<< b << "    value3 : " << c  << endl ;
+    cout << Grade << endl; 
+    cout<< isSafe << endl << isWin ;
+
+
+  return 0;
+
+}

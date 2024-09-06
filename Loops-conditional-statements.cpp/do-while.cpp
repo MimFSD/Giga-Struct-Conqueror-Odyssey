6fe574fbf0ca4708b500762d9f138e7d6d8153ca
MimFSD <mimislam878@gmail.com>
2024-09-06 07:13:24 +0600

    do-while loops

---
+++ Loops-conditional-statements.cpp/do-while.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+    int n = 9 ;
+    int i =1;
+    do
+    {
+        cout<< i <<"      ";
+        i+=1;
+    } while (i<=n);
+    
+    return 0; 
+} 

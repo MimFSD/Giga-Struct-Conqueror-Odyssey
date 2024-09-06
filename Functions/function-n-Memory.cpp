db994ff04043fa65488be04a3a7a4b5c51cb8b8c
MimFSD <mimislam878@gmail.com>
2024-09-07 00:58:57 +0600

    Function in Memory

---
+++ Functions/function-n-Memory.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int fMemory(int g,int h){
+    return g+h;
+}
+int main (){
+    cout<<fMemory(23,34)<<endl;
+    cout<<fMemory(17,14)<<endl;
+
+    return 0 ;
+}

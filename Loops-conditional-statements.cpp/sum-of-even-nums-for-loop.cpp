b3f794145600afa7a9ade0c8f894bb84e7b6252d
MimFSD <mimislam878@gmail.com>
2024-09-06 07:06:34 +0600

    task done

---
+++ Loops-conditional-statements.cpp/sum-of-even-nums-for-loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+    int n=50;
+    int oddSum = 0 ;
+
+   for(int i=1;i<=n;i++){
+       if(i%2 == 0){
+        oddSum += i;
+       }
+    
+    }
+    
+    
+
+cout<<"Sum of even numbers using for loops : "<< oddSum<<endl;
+    return 0; 
+}
--- Loops-conditional-statements.cpp/sum-of-while-odd-numbers.cpp
+++ Loops-conditional-statements.cpp/sum-of-while-odd-numbers.cpp
@@ -1,5 +1,18 @@
+// Sum of Odd number using while loop
+
 #include<iostream>
 using namespace std ;
 int main (){
-    int n=25;
+    int n= 9 , oddSum=0;
+    int i=1;
+    while(i<=n){
+        if (i%2 !=0){
+           oddSum+=i;
+        }
+        i++; 
+        
+    }
+
+    cout<<"Sum of Odd Numbers : " << oddSum;
+
 }

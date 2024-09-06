772037b1c6cfe953312c3358b66dcd74578bc838
MimFSD <mimislam878@gmail.com>
2024-09-06 07:37:11 +0600

    given number – prime or not

---
+++ Loops-conditional-statements.cpp/prime.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int n = 17;
+    bool isPrime = true;
+
+    for (int i=2 ;i<=n-1;i++){
+        if(n%i ==0){
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime==true)
+    {
+        cout<<"Prime Number";
+    }
+    else
+    {
+        cout<<"Non-Prime Number" ;
+    }
+    
+    
+}

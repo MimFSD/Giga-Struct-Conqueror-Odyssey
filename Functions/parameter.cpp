5daeaf5db99e71da50cd9da5607d39c5a0a9b2c3
MimFSD <mimislam878@gmail.com>
2024-09-07 00:19:42 +0600

    parameter

---
+++ Functions/parameter.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+// sum of 2 numbers 
+int sum(int a ,int b){
+    int s = a + b;
+    return s;
+}
+int main(){
+    cout<< sum(45,35) ;
+    return 0 ;
+}

9564122339e0c669d21e233c46399f97d7ab18d4
MimFSD <mimislam878@gmail.com>
2024-09-09 10:55:49 +0600

    Triangle Pattern

---
+++ Patterns/Triangle-pattern.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    // Asking the user for the number of rows in the triangle
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    // Outer loop for each row
+    for (int i = 1; i <= n; i++) {
+        // Inner loop for each column
+        for (int j = 1; j <= i; j++) {
+            cout << "* "; 
+        }
+        cout << endl; 
+    }
+
+    return 0;
+}

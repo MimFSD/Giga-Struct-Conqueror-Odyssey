8a314d3874a4d83e5ed7f438b205bb4922f0d62a
MimFSD <mimislam878@gmail.com>
2024-09-10 07:14:56 +0600

    if a number is power of 2 without any loops

---
+++ Bitwise Operators, Data Type Modifiers & more/power-of-2.cpp
@@ -0,0 +1,25 @@
+// figure out how to find if a number is power of 2 without any loops ---- c++
+
+#include <iostream>
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    return (n > 0) && ((n & (n - 1)) == 0);
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPowerOfTwo(num)) {
+        cout << num << " is a power of 2." << endl;
+    } else {
+        cout << num << " is not a power of 2." << endl;
+    }
+
+    return 0;
+}
+
+

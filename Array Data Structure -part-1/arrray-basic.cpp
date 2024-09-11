8f480ad4590e9a276846b63fbea4a991cf377770
MimFSD <mimislam878@gmail.com>
2024-09-11 22:52:25 +0600

    initialize array without specifying the size

--- Array Data Structure -part-1/arrray-basic.cpp
+++ Array Data Structure -part-1/arrray-basic.cpp
@@ -7,6 +7,10 @@ int main() {
     int numbers[5]; // Declares an integer array named 'numbers' with 5 elements.
     int numbers[5] = {1, 2, 3, 4, 5}; // Declares and initializes the 'numbers' array.
 
+    int numbers[] = {1, 2, 3, 4, 5}; // The size of the array will be automatically determined by the number of elements.
+    
+
+
 
   cout << "Hello World!";
   return 0;

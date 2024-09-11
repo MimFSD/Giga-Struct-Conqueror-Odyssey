5db3b5adb2874a18e0157f15742de3b6f89ae729
MimFSD <mimislam878@gmail.com>
2024-09-11 22:51:18 +0600

    Initializing an Array

--- Array Data Structure -part-1/arrray-basic.cpp
+++ Array Data Structure -part-1/arrray-basic.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int main() {
+    int numbers[5]; // Declares an integer array named 'numbers' with 5 elements.
+    int numbers[5] = {1, 2, 3, 4, 5}; // Declares and initializes the 'numbers' array.
+
+
   cout << "Hello World!";
   return 0;
 }

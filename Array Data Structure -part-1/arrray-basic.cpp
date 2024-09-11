2b75c77560972e6cb41677b7a554aea124bef3a2
MimFSD <mimislam878@gmail.com>
2024-09-11 22:53:56 +0600

    Accessing Array Elements

--- Array Data Structure -part-1/arrray-basic.cpp
+++ Array Data Structure -part-1/arrray-basic.cpp
@@ -8,7 +8,13 @@ int main() {
     int numbers[5] = {1, 2, 3, 4, 5}; // Declares and initializes the 'numbers' array.
 
     int numbers[] = {1, 2, 3, 4, 5}; // The size of the array will be automatically determined by the number of elements.
+
+    int numbers[5] = {10, 20, 30, 40, 50};
+    int firstNumber = numbers[0]; // Accesses the first element (10)
+    int secondNumber = numbers[1]; // Accesses the second element (20)
+
     
+
 
 
 

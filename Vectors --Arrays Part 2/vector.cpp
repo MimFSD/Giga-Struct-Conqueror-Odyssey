bbb2b3987422a001f3921d71307b3b866ce6abdd
MimFSD <mimislam878@gmail.com>
2024-09-15 10:42:43 +0600

    nothing show because empty vector

--- Vectors --Arrays Part 2/vector.cpp
+++ Vectors --Arrays Part 2/vector.cpp
@@ -1,7 +1,15 @@
 // Vectors like array  -->  Dynamic 
-
 //In array , Size Fixed
 // In vectors,  Not fixed Size  -- Size can changed
-
 // Standard Template Library (STL)
 
+
+#include <iostream>
+#include<vector>
+using namespace std ;
+int main (){
+vector <int> vec  ;    // 0
+
+cout <<vec[0] ;  //nothing show because empty vector
+    return 0;
+}

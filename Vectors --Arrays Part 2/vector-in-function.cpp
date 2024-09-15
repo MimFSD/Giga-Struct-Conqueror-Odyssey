6aae4d9ce2094ea5d0783f19a5236c45e9d18e1a
MimFSD <mimislam878@gmail.com>
2024-09-15 11:27:42 +0600

    example --push_back , pop_back ,front ,back at

--- Vectors --Arrays Part 2/vector-in-function.cpp
+++ Vectors --Arrays Part 2/vector-in-function.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main() {
     vector<int> vec;
 
-    // Adding elements to the vector
     vec.push_back(10);   // vec = [10]
     vec.push_back(20);   // vec = [10, 20]
     vec.push_back(30);   // vec = [10, 20, 30]
@@ -15,7 +14,6 @@ int main() {
     cout << "Last element (back): " << vec.back() << endl;     // Output: 30
     cout << "Element at index 1: " << vec.at(1) << endl;       // Output: 20
 
-    // Removing the last element
     vec.pop_back();  // vec = [10, 20]
     cout << "Last element after pop_back: " << vec.back() << endl;  // Output: 20
 

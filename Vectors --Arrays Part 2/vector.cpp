82ba38459c621abc7cc479347d7147acce80f139
MimFSD <mimislam878@gmail.com>
2024-09-15 10:57:28 +0600

    set & same value vector

--- Vectors --Arrays Part 2/vector.cpp
+++ Vectors --Arrays Part 2/vector.cpp
@@ -8,8 +8,17 @@
 #include<vector>
 using namespace std ;
 int main (){
-vector <int> vec  ;    // 0
+// vector <int> vec  ;    // 0
+// cout <<vec[0] ;  //nothing show because empty vector
 
-cout <<vec[0] ;  //nothing show because empty vector
+vector <int> ver={7,5,9};
+cout<<ver[0] << endl;
+
+vector <int> vec (5,13);
+cout<<vec[0]  <<endl ;
+cout<<vec[1] <<endl;
+cout<<vec[2] <<endl;
+cout<<vec[3] <<endl;
+cout<<vec[4] <<endl;
     return 0;
 }

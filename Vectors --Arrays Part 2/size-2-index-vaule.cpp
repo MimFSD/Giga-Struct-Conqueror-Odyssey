d42024b32209e3ccb100e696c245c436837e6477
MimFSD <mimislam878@gmail.com>
2024-09-15 11:18:31 +0600

    added for each loop

--- Vectors --Arrays Part 2/size-2-index-vaule.cpp
+++ Vectors --Arrays Part 2/size-2-index-vaule.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     vector <int> vec2 ={'a','d','f','x','s'} ;
-    for (char val : vec2){
+    for (char val : vec2){  // added for each loop
         cout<<val<<endl;
     }
 

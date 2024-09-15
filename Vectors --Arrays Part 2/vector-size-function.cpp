a18caf945584b1f705005c4a0bd9500c502d4542
MimFSD <mimislam878@gmail.com>
2024-09-15 11:21:35 +0600

    size vector

---
+++ Vectors --Arrays Part 2/vector-size-function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> myVector = {10, 20, 30, 40, 50};
+
+
+    cout << "Size of the vector: " << myVector.size() << endl;
+
+   
+
+    return 0;
+}

de45955691c7f66bf7a4294acc1753e51b0d2dba
MimFSD <mimislam878@gmail.com>
2024-09-15 11:16:42 +0600

    Accessing all elements of the vector using a for loo

---
+++ Vectors --Arrays Part 2/size-2-index-vaule.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector <int> vec2 ={'a','d','f','x','s'} ;
+    for (char val : vec2){
+        cout<<val<<endl;
+    }
+
+
+  return 0;
+}

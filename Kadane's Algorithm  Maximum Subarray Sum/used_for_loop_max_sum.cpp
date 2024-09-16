ebfff68abe0214ed781eacf38123245856b50085
MimFSD <mimislam878@gmail.com>
2024-09-16 09:41:12 +0600

    Update max_sum if needed

--- Kadane's Algorithm  Maximum Subarray Sum/used_for_loop_max_sum.cpp
+++ Kadane's Algorithm  Maximum Subarray Sum/used_for_loop_max_sum.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 int maxSubArraySum(int arr[], int n) {
-    int max_sum = arr[0];      // Initialize max_sum with the first element
-    int current_sum = arr[0];  // Initialize current_sum with the first element
+    int max_sum = arr[0];        // Initialize max_sum with the first element
+    int current_sum = arr[0];   // Initialize current_sum with the first element
 
-    // Iterate through the array starting from the second element
+      // Iterate through the array starting from the second element
     for (int i = 1; i < n; i++) {
         current_sum = max(arr[i], current_sum + arr[i]);    // Update current sub-array sum
-        max_sum = max(max_sum, current_sum);              // Update max_sum if needed
+        max_sum = max(max_sum, current_sum);                 // Update max_sum if needed
     }
 
     return max_sum;

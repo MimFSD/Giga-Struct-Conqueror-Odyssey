193f2e5960b6b30d020c66a81ee3f92eeb68d6bd
MimFSD <mimislam878@gmail.com>
2024-09-21 23:32:12 +0600

    change the value for testing

--- Container with Most Water Problem/brute-force.cpp
+++ Container with Most Water Problem/brute-force.cpp
@@ -20,7 +20,7 @@ int maxAreaBruteForce(vector<int>& height) {
 }
 
 int main() {
-    vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    vector<int> height = {1, 8, 6, 2, 3, 4, 8, 9, 7};
     cout << "The maximum area of water the container can store (Brute Force) is: " << maxAreaBruteForce(height) << endl;
 
     return 0;

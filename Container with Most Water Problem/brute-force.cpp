edfca119a0dde219e0c19153b0e973c66167640b
MimFSD <mimislam878@gmail.com>
2024-09-21 23:30:47 +0600

    Update the maximum area found

--- Container with Most Water Problem/brute-force.cpp
+++ Container with Most Water Problem/brute-force.cpp
@@ -8,13 +8,10 @@ int maxAreaBruteForce(vector<int>& height) {
     int n = height.size();
     int max_area = 0;
 
-    // Check all pairs (i, j) where i < j
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
-            // Calculate the area between lines at i and j
             int current_area = min(height[i], height[j]) * (j - i);
             
-            // Update the maximum area found
             max_area = max(max_area, current_area);
         }
     }
@@ -23,7 +20,6 @@ int maxAreaBruteForce(vector<int>& height) {
 }
 
 int main() {
-    // Example usage
     vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
     cout << "The maximum area of water the container can store (Brute Force) is: " << maxAreaBruteForce(height) << endl;
 

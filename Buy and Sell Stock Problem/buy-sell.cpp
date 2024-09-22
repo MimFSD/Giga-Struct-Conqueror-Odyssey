f5caa1e8bcf4ce9fcbec0727ed525db548c306de
MimFSD <mimislam878@gmail.com>
2024-09-22 09:31:32 +0600

    Return the maximum profit found

--- Buy and Sell Stock Problem/buy-sell.cpp
+++ Buy and Sell Stock Problem/buy-sell.cpp
@@ -10,19 +10,14 @@ int maxProfit(vector<int>& prices) {
     int min_price = prices[0];  
     int max_profit = 0;          
 
-    // Traverse the price array
     for (int i = 1; i < prices.size(); ++i) {
-        // Update min_price if the current price is lower than the min_price so far
         min_price = min(min_price, prices[i]);
 
-        // Calculate the current potential profit
         int current_profit = prices[i] - min_price;
-
-        // Update max_profit if the current profit is greater than the previous max_profit
         max_profit = max(max_profit, current_profit);
     }
 
-    return max_profit;  // Return the maximum profit found
+    return max_profit; 
 }
 
 int main() {

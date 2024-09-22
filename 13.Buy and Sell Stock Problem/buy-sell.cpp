500230b5e82ef5aa4e13dee3af183f459f6682f5
MimFSD <mimislam878@gmail.com>
2024-09-22 09:34:39 +0600

    compute the maximum profit that could be achieved at each step

--- 13.Buy and Sell Stock Problem/buy-sell.cpp
+++ 13.Buy and Sell Stock Problem/buy-sell.cpp
@@ -4,29 +4,21 @@
 using namespace std;
 
 int maxProfit(vector<int>& prices) {
-  
     if (prices.empty()) return 0;
-
     int min_price = prices[0];  
     int max_profit = 0;          
-
     for (int i = 1; i < prices.size(); ++i) {
         min_price = min(min_price, prices[i]);
 
         int current_profit = prices[i] - min_price;
         max_profit = max(max_profit, current_profit);
     }
-
     return max_profit; 
 }
 
 int main() {
     vector<int> prices = {7, 1, 5, 3, 6, 4};
-
-    
     int profit = maxProfit(prices);
-
     cout << "Maximum profit: " << profit << endl;
-
     return 0;
 }

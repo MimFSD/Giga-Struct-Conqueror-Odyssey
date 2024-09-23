3ff083ff8594255660cb0e0934e24dd4c6cafcb5
MimFSD <mimislam878@gmail.com>
2024-09-23 08:26:54 +0600

    time complexity of O(n)

--- 11.Majority Element  Brute- Better-Best Approach/pair-sum-optimal.cpp
+++ 11.Majority Element  Brute- Better-Best Approach/pair-sum-optimal.cpp
@@ -3,22 +3,16 @@
 using namespace std;
 
 void findPairWithSum(int arr[], int n, int targetSum) {
-    unordered_set<int> seen; // To store the elements we've already checked
+    unordered_set<int> seen; 
 
     for (int i = 0; i < n; i++) {
         int complement = targetSum - arr[i];
-
-        // If the complement exists in the set, we found a pair
         if (seen.find(complement) != seen.end()) {
             cout << "Pair found: (" << arr[i] << ", " << complement << ")" << endl;
             return;
         }
-
-        // Otherwise, add the current element to the set
         seen.insert(arr[i]);
     }
-
-    // If no pair is found
     cout << "No pair found with sum " << targetSum << endl;
 }
 
@@ -26,7 +20,6 @@ int main() {
     int arr[] = {10, 15, 3, 7};
     int targetSum = 17;
     int n = sizeof(arr) / sizeof(arr[0]);
-
     findPairWithSum(arr, n, targetSum);
 
     return 0;

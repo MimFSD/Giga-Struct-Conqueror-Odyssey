290d1c7f1d357c904b8948716d6df8ad8f735ff4
MimFSD <mimislam878@gmail.com>
2024-09-27 23:26:52 +0600

    sorted array by repeatedly dividing

--- Binary Search Algorithm - Iterative and Recursive Method/binary-search.cpp
+++ Binary Search Algorithm - Iterative and Recursive Method/binary-search.cpp
@@ -8,19 +8,15 @@ int binarySearch(int arr[], int size, int target) {
     while (left <= right) {
         int mid = left + (right - left) / 2; 
 
-        // Check if the target is present at mid
         if (arr[mid] == target)
             return mid;
 
-        // If target is greater, ignore the left half
         if (arr[mid] < target)
             left = mid + 1;
-        // If target is smaller, ignore the right half
         else
             right = mid - 1;
     }
 
-    // Target was not found
     return -1;
 }
 

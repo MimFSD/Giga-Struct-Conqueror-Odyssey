4b698b0242ad73b9858412b74d41c9fb6973829e
MimFSD <mimislam878@gmail.com>
2024-09-28 12:08:42 +0600

    Dry Run of Binary

--- Binary Search Algorithm - Iterative and Recursive Method/dry-run-binary.cpp
+++ Binary Search Algorithm - Iterative and Recursive Method/dry-run-binary.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+using namespace std;
+
 int binarySearch(int arr[], int size, int target) {
     int low = 0;
     int high = size - 1;
@@ -6,7 +9,7 @@ int binarySearch(int arr[], int size, int target) {
         int mid = low + (high - low) / 2;
 
         if (arr[mid] == target) {
-            return mid;  // Target found
+            return mid;  
         }
         else if (arr[mid] < target) {
             low = mid + 1;  // Search in the right half
@@ -17,4 +20,19 @@ int binarySearch(int arr[], int size, int target) {
     }
 
     return -1;  // Target not found
+}
+
+int main() {
+    int arr[] = {2, 3, 4, 10, 40};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 10;
+    int result = binarySearch(arr, size, target);
+
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
 }
